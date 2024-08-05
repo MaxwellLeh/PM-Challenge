30259c22ec42951fb18f88aac7a045cfad7ef208
MaxwellLeh <max.lehain@gmail.com>
2024-08-05 09:48:13 +0100

    first-commit

---
+++ api/database/setup.js
@@ -0,0 +1,14 @@
+// require("dotenv").config();
+// const fs = require("fs");
+
+// console.log(__dirname);
+
+// const db = require("./connect");
+// const sql = fs.readFileSync(__dirname + "/setup.sql").toString();
+
+// db.query(sql)
+//   .then((data) => {
+//     db.end();
+//     console.log("Set up complete");
+//   })
+//   .catch((error) => console.log(error));
