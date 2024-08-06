d369cfbf4db1f8af641b12d8bbc3a3a6ac1a4637
thenushan <thenushan.sivanesan@gmail.com>
2024-08-06 15:17:47 +0100

    added function in model

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/user");
+const User = require("./models/User.js");
 
 async function register(req, res) {
   try {
