f87d3fdddf76004dba5b9594c9d0b7be3b712b7e
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-08-07 00:01:00 +0100

    Fixed app.js

--- api/app.js
+++ api/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors")
 const logger = require("./middleware/logger")
 
-//const usersRouter = require("./api/routers/usersRouter");
-//const countriesRouter = require("./api/routers/countriesRouter");
-//const quizResultsRouter = require("./api/routers/quizResultsRouter");
-//const feedbackRouter = require("./api/routers/feedbackRouter");
-//const quizQuestionsRouter = require("./api/routers/quizQuestionsRouter");
+const usersRouter = require("./api/routers/usersRouter");
+const countriesRouter = require("./api/routers/countriesRouter");
+const quizResultsRouter = require("./api/routers/quizResultsRouter");
+const feedbackRouter = require("./api/routers/feedbackRouter");
+const quizQuestionsRouter = require("./api/routers/quizQuestionsRouter");
 const leaderboardRouter = require("./routers/leaderboardRouter")
 
 const app = express();
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(cors())
 app.use(logger)
 
-//app.use("/users", usersRouter);
-//app.use("/countries", countriesRouter);
-//app.use("/quiz-results", quizResultsRouter);
-//app.use("/feedback", feedbackRouter);
-//app.use("/quiz-questions", quizQuestionsRouter);
+app.use("/users", usersRouter);
+app.use("/countries", countriesRouter);
+app.use("/quiz-results", quizResultsRouter);
+app.use("/feedback", feedbackRouter);
+app.use("/quiz-questions", quizQuestionsRouter);
 app.use("/leaderboard", leaderboardRouter)
 
 app.get("/", (req, res) => {
