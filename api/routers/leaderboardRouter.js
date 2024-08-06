82e4e1a72951b1732ef93ced2560c1d6947d70f4
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-08-06 19:54:06 +0100

    Edited rank in .sql

--- api/routers/leaderboardRouter.js
+++ api/routers/leaderboardRouter.js
@@ -3,8 +3,6 @@ const { Router } = require('express');
 const leaderboardController = require('../controllers/leaderboardController');
 const leaderboardRouter = Router();
 
-
-leaderboardRouter.get("/leaderboard", leaderboardController.index);
 leaderboardRouter.get("/leaderboard/:id", leaderboardController.show);
 leaderboardRouter.patch("/leaderboard/:id", leaderboardController.update);
 leaderboardRouter.post("/leaderboard", leaderboardController.create);
