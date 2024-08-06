b5be38cd27ac83285a62f52dc0818a75d10746f7
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-08-07 00:04:49 +0100

    Fixed app.js

--- api/app.js
+++ api/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
-<<<<<<< HEAD
 const cors = require("cors")
 const logger = require("./middleware/logger")
 
-=======
+
 
 const usersRouter = require("./routers/usersRouter");
->>>>>>> 5b91687f90b450260a6e480d41d93645fc4e3eee
 const countriesRouter = require("./routers/countriesRouter");
 const quizResultsRouter = require("./routers/quizResultsRouter");
 const feedbackRouter = require("./routers/feedbackRouter");
