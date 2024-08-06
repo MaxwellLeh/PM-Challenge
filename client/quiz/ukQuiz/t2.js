c583b21e3339ea91b00de9d051ab3047330972a7
php20ai <aliah.iqbal123@hotmail.com>
2024-08-06 09:33:20 +0100

    Create questions in json file.

---
+++ client/quiz/ukQuiz/t2.js
@@ -0,0 +1,14 @@
+
+const quizJson = require('./quiz.json')
+for (let i=0; i<quizJson.length;i++){
+    
+    console.log(quizJson[i])
+}
+   // const b=document.querySelector("#form")
+   // b.addEventListener('submit', (e)=>{
+     //   e.preventDefault()
+       // const option = document.querySelector("#form #option1")
+        //option.value= "Myname"
+    //})
+    
+    
