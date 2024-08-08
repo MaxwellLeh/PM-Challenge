fd8826bbbf14cfe5dfbbfce1dca20e167f37c9d5
MaxwellLeh <max.lehain@gmail.com>
2024-08-08 21:27:14 +0100

    working

--- client/assets/leaderboard.js
+++ client/assets/leaderboard.js
@@ -15,11 +15,24 @@ async function load() {
   const resp = await response.json();
   console.log({ resp });
   resp.data.forEach((student) => {
+    console.log("leaderboard.js", student.rank);
     const tr = document.createElement("tr");
     const tdName = document.createElement("td");
     tdName.textContent = student.name;
     const tdRank = document.createElement("td");
-    tdRank.textContent = student.rank;
+    if (student.rank === 1){
+      tdRank.textContent = "Prime Minister"
+    } else if (student.rank == 2) {
+      tdRank.textContent = "Deputy Prime Minister"
+    } else if (student.rank == 3) {
+      tdRank.textContent = "Minister"
+    } else if (student.rank == 4) {
+      tdRank.textContent = "Governor"
+    } else {
+      student.rank == 5
+      tdRank.textContent = "Mayor"
+    }
+    // tdRank.textContent = student.rank;
     const tdScore = document.createElement("td");
     tdScore.textContent = student.score;
     tr.appendChild(tdName);
@@ -29,3 +42,4 @@ async function load() {
   });
 }
 load();
+ 
