e1bf1669fd4812b46d2cbf1b927df13800ea65ad
Poongodi <poongodi@Poongodis-MBP.lan>
2024-08-08 07:31:46 +0100

    Leaderboard updation done

---
+++ client/assets/gbQuiz.js
@@ -0,0 +1,71 @@
+const form = document.querySelector(".form");
+let questions = [];
+let currentQuestion = [];
+let score = 0;
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const clicked = e.submitter.textContent;
+  const index = questions.indexOf(currentQuestion);
+  // if send after every question fetch request here
+  if (index + 1 !== questions.length) {
+    if (clicked === currentQuestion.correct_answer) {
+      score++;
+    }
+    currentQuestion = questions[index + 1];
+    updatedom();
+  } else {
+    await saveResults(score, 2, Math.floor(score / questions.length));
+    // if send at end to results fetch request here
+    window.location.assign("leaderboard.html");
+  }
+});
+const saveResults = async (score, country_id, rank) => {
+  const options = {
+    method: "POST",
+    headers: {
+      authorization: localStorage.getItem("token"),
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      total_score: score,
+      country_id: country_id,
+      rank: rank,
+    }),
+  };
+  const response = await fetch(
+    "http://127.0.0.1:3000/quiz-results/save",
+    options
+  );
+  console.log("save result ", response);
+  return response.ok;
+};
+const fetchQuestions = async () => {
+  const response = await fetch("http://127.0.0.1:3000/quiz-questions/2");
+
+  if (!response.ok) {
+    console.log("err");
+  }
+
+  const data = await response.json();
+  questions = data;
+  currentQuestion = questions[0];
+  updatedom();
+};
+
+fetchQuestions();
+
+const updatedom = () => {
+  const titleEl = document.querySelector(".question");
+  titleEl.textContent = currentQuestion.question_text;
+
+  const scoreEl = document.querySelector(".score");
+  scoreEl.textContent = score;
+
+  const { correct_answer, option_2, option_3, option_4 } = currentQuestion;
+  const options = [correct_answer, option_2, option_3, option_4];
+  const elements = document.querySelectorAll(".quiz-buttons");
+  elements.forEach((el, i) => {
+    el.textContent = options[i];
+  });
+};
